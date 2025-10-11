48f8782680d8d4b2ed6708e7233e2ac4bb0d85af
雨落心弦 <489825920@qq.com>
2025-10-11 10:12:59 +0800

    1.创建用户类，需要包含用户的基本属性，包含账号，密码，用户名，邮箱，手机号，地址，性别，出生年月，用户状态等基本字段； 2.完成用户的新增，删除，修改，查询接口。 3.完成登录、忘记密码、修改密码接口。

---
+++ test1/test/src/main/java/com/test/test/result/Result.java
@@ -0,0 +1,59 @@
+package com.test.test.result;
+
+import lombok.Data;
+
+import java.io.Serializable;
+
+
+@Data
+public class Result<T> implements Serializable {
+
+    /**
+     * 1成功 0失败
+     */
+
+    private Integer code;
+
+
+    private String msg;
+
+
+    private T data;
+
+    public static <T> Result<T> success() {
+        Result<T> result = new Result<T>();
+        result.code = 1;
+        return result;
+    }
+
+    public static <T> Result<T> success(T object) {
+        Result<T> result = new Result<T>();
+        result.data = object;
+        result.code = 1;
+        return result;
+    }
+
+    public static <T> Result<T> success(String msg, T object) {
+        Result<T> result = new Result<T>();
+        result.msg = msg;
+        result.data = object;
+        result.code = 1;
+        return result;
+    }
+
+    public static <T> Result<T> error(String msg) {
+        Result result = new Result();
+        result.msg = msg;
+        result.code = 0;
+        return result;
+    }
+
+    public static <T> Result<T> error(String msg,T object) {
+        Result result = new Result();
+        result.msg = msg;
+        result.code = 0;
+        result.data = object;
+        return result;
+    }
+
+}
